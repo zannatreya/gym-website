e8b1cda92cb5a7de4cc96072d6b218992b0e458d
zannatreya <zannatulbintabahar@yahoo.com>
2022-09-25 01:31:01 +0600

    hero section done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Header from '../Header/Header';
-import './Hero.css';
+import React from "react";
+import Header from "../Header/Header";
+import "./Hero.css";
 import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import heart from "../../assets/heart.png";
@@ -8,74 +8,84 @@ import calories from "../../assets/calories.png";
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <div className="left-h">
-            <Header/>
+    <div className="hero">
+      <div className="left-h">
+        <Header />
 
-            {/* the best ad */}
+        {/* the best ad */}
 
-            <div className="the-best-ad">
-                <div></div>
-                <span>the best fitness club in the town</span>
-            </div>
-
-            {/* Hero Heading */}
-
-            <div className="hero-text">
-              <div>
-                <span className='stroke-text'>Shape </span>
-                <span>Your</span>
-              </div>
-              <div>
-                <span>Ideal body</span>
-              </div>
-              <div>
-                <span>
-                In here we will help you to shape and build your ideal body 
-                and live up your life to fullest.
-
-                </span>
-              </div>
-            </div>
-
-            {/* figures */}
-
-            <div className="figures">
-              <div>
-                <span>+140</span>
-                <span>expert coachs</span>
-              </div>
-              <div>
-                <span>+978</span>
-                <span>members joined</span>
-              </div>
-              <div>
-                <span>+50</span>
-                <span>fitness programs</span>
-              </div>
-            </div>
-
-            {/* hero button */}
-
-            <div className="hero-buttons">
-              <button className='btn'>Get Started</button>
-              <button className='btn'>Learn More</button>
-            </div>
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
         </div>
-        <div className="right-h">
-          <button className='btn'>Join Now</button>
 
+        {/* Hero Heading */}
 
-          <div className='heart-rate'>
-            <img src={heart} alt="" />
-            <span>Heart Rate</span><span>116 bpm</span>
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
           </div>
-        
-        
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest.
+            </span>
+          </div>
         </div>
 
-    </div>
-  )
-}
+        {/* figures */}
 
-export default Hero
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* hero button */}
+
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        {/* hero images */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+
+        {/* calories */}
+
+        <div className="calories">
+          <img src={calories} alt="" />
+          <div>
+            <span>Calories burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
